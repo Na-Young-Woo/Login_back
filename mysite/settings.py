f14548e9d66ca4d6f4add7d1c81d8ccc00ef12ce
woonayoung <dn2255520@gmail.com>
2020-12-17 23:57:37 +0900

    setting 진행 중

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_user',
+
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'mysql',
+        'PASSWORD':'zz7522nb',
+
     }
 }
 
